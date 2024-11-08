0888ea7399f57ee5fba1405a396820a68f821b02
Daylen Jones <jonesday001@warren.k12.in.us>
2024-11-08 12:36:57 -0500

    update

--- src/birthday.java
+++ src/birthday.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class birthday {
-}
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+
+    public class birthday {
+        public static void main(String[] args) {
+
+            LocalDateTime fakeBirthday = LocalDateTime.of(2000, 5, 15, 10, 0, 0);
+
+
+            ZonedDateTime parisTime = fakeBirthday.atZone(ZoneId.of("Europe/Paris"));
+
+
+            System.out.println("Fake Birthday in Paris time: " + parisTime);
+        }
+    }
+
+
